a2c5232f23c63ab20f945dba7d9ddd5ea2951893
wazeveve <gabrielveanancio1123@gmail.com>
2025-11-19 15:17:48 -0300

    Creating API that returns a order

---
+++ GoodHamburguer/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoodHamburguer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet("summary")]
+        public IActionResult GetSummary()
+        {
+            var summary = new
+            {
+                orderId = 1234,
+                customer = "Gabriel",
+                items = new[]
+                {
+                    new { name = "X-Burger", qty = 1, price = 19.90 },
+                    new { name = "Fries",    qty = 2, price = 9.00  },
+                    new { name = "Soft Drink", qty = 1, price = 6.00 }
+                },
+                total = 19.90 + 18 + 6
+            };
+
+            return Ok(summary);
+        }
+    }
+}
